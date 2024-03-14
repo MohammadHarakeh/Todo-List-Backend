d4ac07cc0806e40e47d211e11656858678589cc2
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-14 03:47:05 +0200

    added signin.php where it checks if the user is in the DB

--- backend/signin.php
+++ backend/signin.php
@@ -5,4 +5,24 @@ $email = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-?>
+$query = $mysqli->prepare('select id,email,username,password from users where email=?');
+$query->bind_param('s', $email);
+$query->execute();
+$query->store_result();
+$query->bind_result($id,$email,$username,$hashed_password);
+$query->fetch();
+$num_rows = $query->num_rows();
+
+if($num_rows == 0){
+    $response['status'] = "user not found";
+} else{
+    if(password_verify($password,$hashed_password)){
+        $response['status'] = "logged in";
+        $response['user_id'] = $id;
+        $response['username'] = $username;
+        $response['email'] = $email;
+    } else{
+        $response['status'] = "incorrect credentials";
+    }
+}
+echo json_encode($response);
